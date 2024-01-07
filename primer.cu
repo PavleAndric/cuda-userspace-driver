6832ae24b7714e1876ffcadd5a9f6381a70cca10
PavleAndric <pavle.andric70@gmail.com>
2024-01-07 18:32:14 +0100

    pokazivaci

--- primer.cu
+++ primer.cu
@@ -4,35 +4,51 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <sys/mman.h>
+#include <assert.h>
 
 #define N 100
 
+void dump(){
+	// dump nvidia0 0x200200000-0x200400000
+	printf("IDE_GAS\n");
+	uint32_t *ptr = (uint32_t*)0x200200000;
+	printf("/dev/nvidia0\n");
+	while (ptr != (uint32_t*)0x200400000){ 
+		if (*ptr != 0){
+			printf("%p %x\n ",ptr ,*ptr); 
+		}
+		ptr ++;
+	}
+}
 int main()
 {   
-    int a[N], b[N], c[N];
+    int a[N], b[N], c[N] ,control[N];
     CUdeviceptr d_a, d_b, d_c;
-
+    
     for (int i = 0; i < N; ++i)
     {
-        a[i] = N - i;
-        b[i] = i * i;
+        a[i] = i;
+        b[i] = i;
+        control[i] = i + i;
     }
     char buf[1500];
     pid_t pid = getpid();
     char path[100];
+
     sprintf(path , "/proc/%d/maps" , pid);
 
     printf("*************cuda_init*************\n");
-    cuInit(0); // MMAP 0x200000000
+    cuInit(0); // MMAP 0x200000000-300200000 uvek ! ! ! 
     printf("*************end_cudainit*************\n");
 
     printf("*************cuda_device_get*************\n");
     CUdevice device;
     cuDeviceGet(&device, 0);
-
     printf("*************cuda_create_contex*************\n");
     CUcontext context;
     cuCtxCreate(&context, 0, device);
+    
     printf("*************cuda_module_load*************\n");
     CUmodule module;
     const char *module_file = "kernel.ptx";
@@ -42,12 +58,30 @@ int main()
     const char *kernel_name = "kernel";
     cuModuleGetFunction(&function, module, kernel_name);
 
+		/*
+    //dev/nvidia0
+    munmap((void*)0x200200000 , 0x200000); mora            
+
+    // /dev/nvidiactl 
+    munmap((void*)0x200400000 , 0x203c00000-0x200400000);   mora
+    munmap((void*)0x204a00000 , 0x204c00000-0x204a00000);   mora
+    munmap((void*)0x205600000 , 0x205800000-0x205600000);   mora
+    munmap((void*)0x204c00000 , 0x204e00000-0x204c00000);   // ne
+    munmap((void*)0x204e00000 , 0x205000000-0x204e00000);   // ne
+    munmap((void*)0x205200000 , 0x205400000-0x205200000);   // ne*/
+			
+		//mprotect((void*)0x200200000 ,0x200400000-0x200200000, PROT_READ);
+    // dump nvidia0 0x200200000-0x200400000
+
+		//mprotect((void*)0x204600000, 0x204800000-0x204600000, PROT_READ);
+
     printf("*************cuda_malloc_1*************\n");
     cuMemAlloc(&d_a, sizeof(int) * N);
     printf("*************cuda_malloc_2*************\n");
     cuMemAlloc(&d_b, sizeof(int) * N);
     printf("*************cuda_malloc_3*************\n");
     cuMemAlloc(&d_c, sizeof(int) * N);
+
     printf("*************cuda_memcpyHtod_1*************\n");
     cuMemcpyHtoD(d_a, a, sizeof(int) * N);
     printf("*************cuda_memcpyHtod_2*************\n");
@@ -59,6 +93,8 @@ int main()
     
     printf("*************cuda_memcpyDtoh*************\n");
     cuMemcpyDtoH(c, d_c, sizeof(int) * N);
+    for(int i = 0 ; i < N ; i ++){assert(c[i] == control[i]);}
+    
     // Free device memory
     printf("*************cuda_Free_1*************\n");
     cuMemFree(d_a);
@@ -67,10 +103,42 @@ int main()
     printf("*************cuda_Free_3*************\n");
     cuMemFree(d_c);
 
-    printf(":D :D :D\n");
+		printf(":D :D :D\n");
     FILE *f = fopen(path, "r");
-	while (fgets(buf, sizeof(buf), f) != NULL){printf("%s", buf);}
-	printf("\n");
-	fclose(f);
+		while (fgets(buf, sizeof(buf), f) != NULL){printf("%s", buf);}
+		printf("\n");
+		fclose(f);
+		
+		dump();
     return 0;
 }
+
+/*
+0x7f719c7de000 /dev/zero
+0x7f719c7de000 /dev/nvidiactl
+0x205000000 /dev/nvidia-uvm
+0x205000000 /dev/nvidia-uvm
+0x7f7196600000 /dev/nvidiactl
+0x7f7196600000 /dev/zero
+0x7f7196600000 /dev/nvidiactl
+0x7f7196600000 /dev/zero
+0x7f7196600000 /dev/nvidiactl
+0x7f7196600000 /dev/zero
+0x7f7196800000 /dev/zero
+0x7f7196800000 /dev/zero
+0x7f7196800000 /dev/zero
+0x7f7196800000 /dev/zero
+0x7f7196800000 /dev/zero
+0x7f7196800000 /dev/zero
+0x7f719c5de000 /dev/zero
+0x7f719c5de000 /dev/zero
+0x205a00000 /dev/zero
+0x205a00000 /dev/zero
+0x205a00000 /dev/zero
+0x7f7197200000 /dev/nvidiactl
+0x7f7197200000 /dev/zero
+0x7f7197200000 /dev/nvidiactl
+0x7f7197200000 /dev/zero
+0x7f7197200000 /dev/nvidiactl
+0x7f7197200000 /dev/zero
+*/
--- proba.py
+++ proba.py
@@ -1,7 +1,23 @@
-import subprocess ,sys ,ctypes ,re
+import subprocess, re
 def print_strgins(pid):
   result = subprocess.run(f"cat /proc/{pid[0]}/maps", shell=True, stdout=subprocess.PIPE, text=True)
   print(result.stdout)
+
+def get_ranges(string , mapping_low,  names):
+  ptrs = [x.split(" ") for x in string.split("\n")]
+  ptrs = [x[1:] if len(x) ==3 else x for x in ptrs[:-1] if len(x) >= 2]
+  all_ = set()
+
+  for x,n in zip(mapping_low, names):
+    y = x[:len("200000000-200200000")]
+    r_1 , r_2 = y.split("-")
+    for k in ptrs:
+      if hex(int(r_1 , 16)) <= (lmao := hex(int(k[-1],16) + int("0x200000000",16))) < hex(int(r_2 , 16)):
+        print(f"0x{r_1} {lmao} 0x{r_2}", n if "/" in n else ("" if r_1 != "200000000" else "Init"))
+        all_.add(lmao)
+
+  esketit = set([hex(int(x[-1], 16) + int("0x200000000",16)) for x in ptrs])
+  assert all_ == esketit 
 
 def all_params(string):
   params = re.compile(r'\b(?:[0-9a-fA-F]{8}|0x[0-9a-fA-F]{8,})\b')
@@ -23,6 +39,11 @@ def get_info(maps, params):
 if __name__  == "__main__":
   file = open("ide_gas.txt", "r").read().split(":D :D :D")
   sniff, mapping = file
-  print(mapping)
-  get_info(mapping.split("\n") ,sniff.split("\n"))
+  mapping, ranges=  mapping.split("IDE_GAS")
+  mapping_low = [x for x in mapping.split("\n") if x and x[0] == "2"]
+  names = [x[-len("/dev/zero (deleted)"):] for x in mapping_low]
+
+  get_ranges(ranges , mapping_low , names)
+  #len("200000000-200200000")
+  #get_info(mapping.split("\n") ,sniff.split("\n"))
   
