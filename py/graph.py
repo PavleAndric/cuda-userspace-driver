fe44c8f09b5237113b16380a67408abc35a266dc
PavleAndric <pavle.andric70@gmail.com>
2024-02-07 21:47:03 +0100

    radi

--- py/graph.py
+++ py/graph.py
@@ -1,10 +1,4 @@
-file = open("../sve.txt" , "r").read().split("\n")
-need = ["NV_ESC_RM_ALLOC" , "NV_ESC_RM_CONTROL"] # "NV_ESC_RM_MAP_MEMORY" ,
-n = [x for x in file if any([y in x for y in need])]
-need = ["hObject","pObjparent","pObjnew"]
-
 glob_graph, cmnd_graph= {} , {}
-
 
 class Object():
   def __init__(self , name:str , type = None, childern:list = []):
@@ -13,8 +7,7 @@ class Object():
   
   def print_all(self):
     self.pprint()
-    for x in self.childern:
-      x.print_all()
+    for x in self.childern: x.print_all()
 
   def get_commands(self): print(self.cmd) if self.cmd else print(end ="")
 
@@ -23,6 +16,7 @@ class Object():
     t_print += " " * (5-len(t_print)) 
     print(f"Object {t_print}: {self.name}" , end=": children{")
     
+    # ovo je djubre
     for i in range(len(self.childern)):
       if i % 4 == 0: print("\n\t\t       " ,end="")
       print(f"{self.childern[i].name} " , end="")
@@ -35,26 +29,28 @@ class Object():
         print(f"{self.cmd[i]} " , end="")
       print("}")
 
+
 def get_cmnd(x):
   str = ""
   for i in range(5, len(x)):
     if x[i] != " ": str += x[i]
     else: return str 
   
-def make_graphs(n): # control
+def make_graphs(n):
   for line in n:
     if "NV_ESC_RM_CONTROL" in line:
       split = line.split(",")
-      romb = [x for x in split if any([y in x for y in need])] + [split[-1]] # ispravi ovo
+      romb = [x for x in split if "hObject" in x] + [split[-1]]
       obj, cmnd = romb[0].split('=')[-1] , get_cmnd(romb[-1])
-      if obj not in cmnd_graph:
-        cmnd_graph[obj] = []
+
+      if obj not in cmnd_graph: cmnd_graph[obj] = []
       cmnd_graph[obj].append(cmnd)
   
   for line in n:
     split = line.split(",")
     romb = [x for x in split if any([y in x for y in need])] + [split[-1]]
 
+    print(romb)
     if any(["pObjparent" in z for z in romb]):
       parent ,child, type = romb[0].split("=")[-1] ,romb[1].split("=")[-1] , romb[-1].split("=")[-1]
       if parent == "0" or child == "0":continue
@@ -70,8 +66,16 @@ def make_rel(k ,type, root = False):
     obj.childern = [make_rel(x[0], x[1], root = False) for x in glob_graph[k]]
   return obj
 
-make_graphs(n)
-root = make_rel(list(glob_graph.keys())[0] , type = None, root = True) 
-root.print_all()
+if __name__ == "__main__":
+  file = open("../sve.txt" , "r").read().split("\n")
+  n = [x for x in file if any([y in x for y in ["NV_ESC_RM_ALLOC" , "NV_ESC_RM_CONTROL" , "NV_ESC_RM_MAP_MEMORY"]])]
+  need = ["hObject","pObjparent","pObjnew",] # 
+
+  #for x in n: print(x)
+  #exit(1)
+  
+  make_graphs(n)
+  root = make_rel(list(glob_graph.keys())[0] , type = None, root = True) 
+  #root.print_all()
 
 
