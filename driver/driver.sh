46a351990d2dddf39b5a9046bf62d15c106a90fb
PavleAndric <pavle.andric70@gmail.com>
2024-03-18 13:17:19 +0100

    mala ispravka

--- driver/driver.sh
+++ driver/driver.sh
@@ -2,8 +2,6 @@ HEADER_PATH_1="../../ide_cuda/open-gpu-kernel-modules/src/nvidia/arch/nvalloc/un
 HEADER_PATH_2="../../ide_cuda/open-gpu-kernel-modules/src/common/sdk/nvidia/inc"
 HEADER_PATH_3="../../ide_cuda/open-gpu-kernel-modules/kernel-open/nvidia-uvm"
 HEADER_PATH_4="../../ide_cuda/open-gpu-kernel-modules/kernel-open/common/inc"
-#//HEADER_PATH_4="../../ide_cuda/open-gpu-kernel-modules/kernel-open/common/inc/"
-
 
 clang++ driver.cc  -I"$HEADER_PATH_1"  -I"$HEADER_PATH_2" -I"$HEADER_PATH_3" -I"$HEADER_PATH_4"  -o driver.o && ./driver.o
 #LD_PRELOAD=./intercept.so ./driver.o
